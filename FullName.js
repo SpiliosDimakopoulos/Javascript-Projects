675d7eadb09f9ad89406b250a27e02aceb3ba4c6
Spilios Dimakopoulos <150211937+SpiliosDimakopoulos@users.noreply.github.com>
2024-11-22 09:35:20 +0200

    Update FullName.js

--- FullName.js
+++ FullName.js
@@ -6,12 +6,16 @@ const rl = readline.createInterface({
 });
 
 rl.question('Enter your full name: (Like: "Spilios Dimakopoulos"): ', (input) => {
-    let trimedInput = input.trim();
-    let firstName = trimedInput.slice(0, trimedInput.indexOf(" "));
-    let lastName = trimedInput.slice(trimedInput.indexOf(" ") + 1);
-    console.log(`This is what the orinal input: ${input}`);
+    let trimmedInput = input.trim();
+    let firstName = trimmedInput.slice(0, trimmedInput.indexOf(" "));
+    let lastName = trimmedInput.slice(trimmedInput.indexOf(" ") + 1);
+
+    firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+    lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
+
+    console.log(`This is what the original input: ${input}`);
     console.log(`This is what the first name: ${firstName}`);
     console.log(`This is what the last name: ${lastName}`);
+    
     rl.close();
 });
-
