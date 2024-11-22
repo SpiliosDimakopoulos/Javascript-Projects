41e1bdb9359dd2143ea5eaf3fbd9c9740c9b2949
Spilios Dimakopoulos <150211937+SpiliosDimakopoulos@users.noreply.github.com>
2024-11-22 10:11:59 +0200

    Update guessNum.js

--- guessNum.js
+++ guessNum.js
@@ -1,28 +1,28 @@
 const minNum = 1;
 const maxNum = 100;
-answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
 
-let quess;
-let attempts;
+let guess;
+let attempts = 0;
 let running = true;
 
-while(running) {
-    guess = window.prompt(`Guess a number between ${minNum} and ${maxNum}.`)
-    guess = Number(guess)
+while (running) {
+    guess = window.prompt(`Guess a number between ${minNum} and ${maxNum}.`);
+    guess = Number(guess);
 
-    if(isNaN(guess)) {
-        window.prompt(`Invalid! Enter your guess.`);
-    } else if(guess << minNum || guess >> maxNum) {
-        window.prompt(`Invalid! Enter your guess, it must be between ${minNum} and ${maxNum}.`);
+    if (isNaN(guess)) {
+        alert(`Invalid! Enter your guess as a number.`);
+    } else if (guess < minNum || guess > maxNum) {
+        alert(`Invalid! Your guess must be between ${minNum} and ${maxNum}.`);
     } else {
-        ++attempts
-        if (guess >> answer) {
-            window.prompt(`Too low. Try again!`);
-        } else if (guess << answer) {
-            window.prompt(`Too high. Try again!`);
+        attempts++;
+        if (guess < answer) {
+            alert(`Too low. Try again!`);
+        } else if (guess > answer) {
+            alert(`Too high. Try again!`);
         } else {
-            window.prompt(`Well Done! You found the correct number! (You win nothing btw). It took you ${attempts}!`);
+            alert(`Well Done! You found the correct number! (You win nothing btw). It took you ${attempts} attempts!`);
+            running = false; // End the loop
         }
     }
-    running = false;
 }
